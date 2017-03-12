28cff7eed1a1b458c800727f8cd7414feac228f6
Grapperon <antoine.grapperon@free.fr>
2017-03-11 20:46:03 -0500

    commit before Charles fucks things up

--- html/main.js
+++ html/main.js
@@ -33,6 +33,10 @@ map.on('load', function () {
     "type": "geojson",
     "data": "https://raw.githubusercontent.com/v3c70r/Maillot-a-pois/master/data/osm_sidewalk_count.geojson"
   });
+  map.addSource("mixed_use", {
+    "type": "geojson",
+    "data": "https://raw.githubusercontent.com/v3c70r/Maillot-a-pois/master/data/osm_mixed_use.geojson"
+  });
 
   // Adding layers from data
   map.addLayer({
@@ -40,7 +44,7 @@ map.on('load', function () {
     "type": "line",
     "source": "bike",
     "paint": {
-      "line-color": "#ff69b4",
+      "line-color": "#3DFF01",
       "line-width": 2
     }
   });
@@ -48,6 +52,10 @@ map.on('load', function () {
     "id": "pedestrian",
     "type": "line",
     "source": "pedestrian",
+    "paint": {
+      "line-color": "#3DFF01",
+      "line-width": 2
+    }
   });
 
   map.addLayer({
@@ -58,13 +66,32 @@ map.on('load', function () {
       "line-color": "#0000ff",
       "line-width": 2
     }
-  });
+  }, 'place-city-sm'  );
+
+  map.addLayer({
+    "id": "link_selected",
+    "type": "line",
+    "source": "active_transportation",
+    "paint": {
+      "line-color": "#ff0000",
+      "line-width": 4
+    },
+    "filter": ["in", "FIPS", ""]
+  }, 'place-city-sm');
   map.addLayer({
     "id": "elevation",
     "type": "line",
     "source": "elevation",
     "paint": {
-      "line-color": "#000000",
+      "line-color": {
+        "property": "denivele",
+        "type": "exponential",
+        "stops": [
+            [0, "#3DFF01"],
+            [10, "#E8A60C"],
+            [20, "#FF0000"]
+        ]
+      },
       "line-width": 2
     }
   });
@@ -78,9 +105,27 @@ map.on('load', function () {
     }
   });
   map.addLayer({
-    "id": "freeways",
+    "id": "sidewalk",
     "type": "line",
-    "source": "freeways",
+    "source": "sidewalk",
+    "paint": {
+      "line-color": {
+        "property": "nb_trottoirs",
+        "type": "categorical",
+        "stops": [
+            [0, "#FF0000"],
+            [1, "#E8A60C"],
+            [2, "#3DFF01"]
+        ]
+      },
+      "line-width": 2
+    }
+  });
+
+  map.addLayer({
+    "id": "Mixed use",
+    "type": "line",
+    "source": "mixed_use",
     "paint": {
       "line-color": "#ff69b4",
       "line-width": 2
@@ -93,7 +138,7 @@ map.on('load', function () {
   map.setCenter([-71.2763062602738, 46.785609176075724]);
 });
 
-var toggleableLayerIds = ['bike', 'pedestrian', 'active_transportation', 'elevation', 'freeways', 'sidewalk'];
+var toggleableLayerIds = ['bike', 'pedestrian', 'active_transportation', 'elevation', 'freeways', 'sidewalk', "Mixed use"];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
     var id = toggleableLayerIds[i];
@@ -137,12 +182,12 @@ $(document).ready(function(){
   });
 
   $(".like").click(function(){
-    $.post("http://10.128.165.20:8080/updateLink", 
-      '{"link_id": 1024, "num_likes": 1, "num_dangerous": 0, "num_damaged": 0}')
-    /*$.get("http://10.128.165.20:8080/getLink/1024", function(data){
+    /*$.post("http://10.128.165.20:8080/updateLink", 
+      '{"link_id": 1024, "num_likes": 1, "num_dangerous": 0, "num_damaged": 0}')*/
+    $.get("http://10.128.165.20:8080/getLink/1024", function(data){
       $("#selected_link").text('tesefefeft');
        alert(data);
-    });*/
+    });
   });
 
   $(".damaged").click(function(){
@@ -155,8 +200,27 @@ $(document).ready(function(){
       '{"link_id": 1024, "num_likes": 0, "num_dangerous": 1, "num_damaged": 0}')
   });
 
+  map.on('click', function(e) {
+    // set bbox as 5px reactangle area around clicked point
+    var box_size = 20;
+    var bbox = [[e.point.x - box_size , e.point.y - box_size], [e.point.x + box_size, e.point.y + box_size]];
+    var features = map.queryRenderedFeatures(bbox, { layers: ['active_transportation'] });
 
+    // Run through the selected features and set a filter
+    // to match features with unique FIPS codes to activate
+    // the `counties-highlighted` layer.
 
+    try{
+      var link_data = features[0].properties.id_unique
+    }
+    catch(err){
+      var link_data = null
+    }
+    
+
+    document.getElementById('selected_link_dangerous').innerHTML = link_data; /*JSON.stringify(features, null, 2);*/
+    
+  });
 
 });
 
@@ -227,20 +291,28 @@ var layout = {barmode: 'stack',
 Plotly.newPlot('company_ranking', data, layout);
 
 
-/*map.on('click', function(e) {
-  // set bbox as 5px reactangle area around clicked point
-  var bbox = [[e.point.x - 5, e.point.y - 5], [e.point.x + 5, e.point.y + 5]];
-  var features = map.queryRenderedFeatures(bbox, { layers: ['counties'] });
 
-  // Run through the selected features and set a filter
-  // to match features with unique FIPS codes to activate
-  // the `counties-highlighted` layer.
-  var filter = features.reduce(function(memo, feature) {
-      memo.push(feature.properties.FIPS);
-      return memo;
-  }, ['in', 'FIPS']);
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
 
-  map.setFilter("counties-highlighted", filter);
-});*/
+    if (query === url || query === "") return;
 
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=", 2);
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
 
+        if (!parms.hasOwnProperty(n)) parms[n] = [];
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+var urlString = "http://10.128.165.20:8080/getLink/1024";
+urlParams = parseURLParams(urlString);
+console.log(urlParams);
+alert(urlParams)
